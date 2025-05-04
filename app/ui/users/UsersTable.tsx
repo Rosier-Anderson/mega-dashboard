3e81c963f2385f6716c4297edfd54713d5e4f8de
Anderson <97542626+Rosier-Anderson@users.noreply.github.com>
2025-05-04 10:55:27 +0000

    fixing usersTable component

--- app/ui/users/UsersTable.tsx
+++ app/ui/users/UsersTable.tsx
@@ -3,6 +3,25 @@ import { UserTableHead } from "@/app/lib/constants/constants";
 import { UserActions } from "@/app/ui/users/UserActions";
 import { UserListProps } from "@/app/lib/types/types";
 const UsersTable: React.FC<UserListProps> = ({ query, users }) => {
+  const renderUserRows = () => {
+    // to change this code later when working on it
+    // push to git hub with a bug
+    // Slice the array starting from index 25
+    // let defaultUsers = query.length === 0 ? users.slice(25) : users;
+    //return defaultUsers.filter((user) => {
+    //   <tr key={user.id}>
+    //     <td className="py-2 px-4">{user.name}</td>
+    //     <td className="py-2 px-4">{user.id}</td>
+    //     <td className="py-2 px-4">user</td>
+    //     <td className="py-2 px-4">active</td>
+    //     <td className="py-2 px-4">22 Apr 2025</td>
+    //     <td className="py-2 px-4">
+    //       <UserActions />
+    //     </td>
+    //   </tr>;
+    // });
+  };
+
   return (
     <table className=" w-full tex-sm text-left  ">
       <thead className="  ">
@@ -15,24 +34,7 @@ const UsersTable: React.FC<UserListProps> = ({ query, users }) => {
           <th className="py-2 px-4">{UserTableHead.ACTIONS}</th>
         </tr>
       </thead>
-      <tbody className="">
-        {users.map((user) => {
-          if (query.length === 0) {
-            return (
-              <tr key={user.id} className="">
-                <td className="py-2 px-4 ">{user.name}</td>
-                <td className="py-2 px-4 ">{user.id}</td>
-                <td className="py-2 px-4 ">user</td>
-                <td className="py-2 px-4 ">active</td>
-                <td className=" py-2 px-4 ">22 apr 2025</td>
-                <UserActions />
-              </tr>
-            );
-          }
-        })}
-
-        {/* this tis the user buttons to modify user  */}
-      </tbody>
+      {/* <tbody className="">{renderUserRows()}</tbody> */}
     </table>
   );
 };
