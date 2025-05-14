fa9e485b76f241e2e1132451b7f54c4953bef6c9
Anderson <97542626+Rosier-Anderson@users.noreply.github.com>
2025-05-14 12:05:02 +0000

    add use sever

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,2 +1,10 @@
 'use server';
-export async function createUser(formData: FormData) {}
+export async function createUser(formData: FormData) {
+    const rawFormData = {
+       name: formData.get('name'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+      };
+      // Test it out:
+      console.log(rawFormData);
+}
