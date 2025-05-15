0f15599f27185be419daa049d101ef71b40b3f00
Anderson <97542626+Rosier-Anderson@users.noreply.github.com>
2025-05-15 07:11:46 +0000

    Adding zod Schema and validating data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,39 @@
 'use server';
+import  { z} from 'zod';
+const FromSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  amount: z.number(),
+  status: z.enum(['Paid', 'Pending']),
+  payment: z.enum(['Debit', 'Credit']),
+  id: z.number()
+
+})
+const createUsers = FromSchema.omit({id: true })
 export async function createUser(formData: FormData) {
-    const rawFormData = {
-    //   "id": 1,
+
+const  {name, email, amount, status, payment} = createUsers.parse({
+   name: formData.get('user-name'),
+        email: formData.get('user-email'),
+        // active: formData.get('user-active'), it ll be automacally true not have to implemted by the user 
+        amount: Number(formData.get('user-amount')),
+        status: formData.get('user-status'),
+        // picture will be automatic not by the user 
+        payment: formData.get('user-payment')
+   
+      // Test it out:
+      
+})
+console.log(name,email, amount, status, payment)
+       
+}
+
+
+
+
+
+
+ //   "id": 1,
     // "date": "Apr 7, 2025 5:00 PM",
     // "name": "Herb Durrett",
     // "plan": "Basic Plan",
@@ -11,15 +43,3 @@ export async function createUser(formData: FormData) {
     // "status": "Paid",
     // "picture": "https://logo.clearbit.com/simplemachines.org",
     // "payment method": "Credit"
-    
-        name: formData.get('user-name'),
-        email: formData.get('user-email'),
-        active: formData.get('user-active'),
-        amount: formData.get('user-amount'),
-        status: formData.get('user-status'),
-        // picture will be automatic not by the user 
-        payment: formData.get('user-payment')
-      };
-      // Test it out:
-      console.log(rawFormData);
-}
